6aaab6439d10d994809efe6d054c3f3a0c98a4eb
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2022-12-06 17:52:16 -0600

    huffman begin

---
+++ Huffman/build_tree.h
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+typedef struct Tree{
+        Tree &izq;
+        Tree &der;
+        char _data;
+}Tree;
+void tree();
+
+
+void tree(){
+    string code;
+    string message;
+
+
+}
+
